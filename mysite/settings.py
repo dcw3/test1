bebe895b543a4bc3cee0f917aa0b019e728eaad4
dcw3 <dcw3@princeton.edu>
2018-03-22 19:16:02 -0400

    psycopg2 req

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'mysite/static_root/mysite'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
